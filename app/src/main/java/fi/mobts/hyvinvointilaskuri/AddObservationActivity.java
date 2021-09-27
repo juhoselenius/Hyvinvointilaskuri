40ae6748641e0a346daf2218737ede0bad145a57
Olli Varila <olli.varila@gmail.com>
2021-09-27 13:40:55 +0300

    AddObservationActivity lisätty

---
+++ app/src/main/java/fi/mobts/hyvinvointilaskuri/AddObservationActivity.java
@@ -0,0 +1,14 @@
+package fi.mobts.hyvinvointilaskuri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddObservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_observation);
+    }
+}
--- app/src/main/java/fi/mobts/hyvinvointilaskuri/ObservationActivity.java
+++ app/src/main/java/fi/mobts/hyvinvointilaskuri/ObservationActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ObservationActivity extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class ObservationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observation);
 
-        Intent intent = getIntent();
     }
+
+    public void buttonPressed(View v) {
+        Intent intent = new Intent(this, AddObservationActivity.class);
+
+        startActivity(intent);
+
+    }
+
 }
