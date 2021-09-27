8dbea93bce62f22c9653a2118049069813f56be4
Olli Varila <olli.varila@gmail.com>
2021-09-27 15:00:50 +0300

    ObservationActivity UI

--- app/src/main/java/fi/mobts/hyvinvointilaskuri/AddObservationActivity.java
+++ app/src/main/java/fi/mobts/hyvinvointilaskuri/AddObservationActivity.java
@@ -2,13 +2,28 @@ package fi.mobts.hyvinvointilaskuri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AddObservationActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_observation);
+        Intent intent = getIntent();
+        String chosenActivity = intent.getStringExtra(ObservationActivity.EXTRA);
+        switch (chosenActivity) {
+            case "weight":
+                setContentView(R.layout.activity_add_weight);
+                break;
+            case "height":
+                setContentView(R.layout.activity_add_height);
+                break;
+            case "meal":
+                setContentView(R.layout.activity_add_meal);
+                break;
+        }
+
     }
 }
