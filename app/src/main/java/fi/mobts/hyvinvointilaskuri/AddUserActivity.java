c69878428a5109198b2e271f2d19b15f4daab34f
TommiUp <tuponen@gmail.com>
2021-09-27 11:49:24 +0300

    AddUserActivityyn UI muutoksia

--- app/src/main/java/fi/mobts/hyvinvointilaskuri/AddUserActivity.java
+++ app/src/main/java/fi/mobts/hyvinvointilaskuri/AddUserActivity.java
@@ -2,6 +2,7 @@ package fi.mobts.hyvinvointilaskuri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AddUserActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class AddUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+
+        Intent intent = getIntent();
     }
 }
