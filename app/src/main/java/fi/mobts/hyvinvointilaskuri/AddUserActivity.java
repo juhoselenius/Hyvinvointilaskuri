01629a067fa51f2274b940c7c88ba00167c7a950
TommiUp <tuponen@gmail.com>
2021-09-27 10:48:43 +0300

    Aktiviteetti "AddUserActivity" lisätty

---
+++ app/src/main/java/fi/mobts/hyvinvointilaskuri/AddUserActivity.java
@@ -0,0 +1,14 @@
+package fi.mobts.hyvinvointilaskuri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
