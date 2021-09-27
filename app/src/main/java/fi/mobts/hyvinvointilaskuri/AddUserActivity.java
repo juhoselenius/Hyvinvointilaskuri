b1c275a8263e792a86924734fbb157e88dce78f7
TommiUp <tuponen@gmail.com>
2021-09-27 13:03:15 +0300

    Korjattu UI AddUserActivity -aktiviteetissa. EditTextit sallivat vain lukuja jos on kyseessä paino tai pituus ja vain kirjaimia jos on kyseessä nimi.

--- app/src/main/java/fi/mobts/hyvinvointilaskuri/AddUserActivity.java
+++ app/src/main/java/fi/mobts/hyvinvointilaskuri/AddUserActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddUserActivity extends AppCompatActivity {
+    private EditText editTextName;
+    private EditText editTextWeight;
+    private EditText editTextHeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
 
+        editTextName = findViewById(R.id.editTextProfileName);
+        editTextWeight = findViewById(R.id.editTextWeight);
+        editTextHeight = findViewById(R.id.editTextHeight);
 
         Intent intent = getIntent();
     }
